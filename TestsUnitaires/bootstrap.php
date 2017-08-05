b60e8924e01efa1a5068dcdd2f212e81656ec69f
Baraban35 <annie.baraban@gmail.com>
2017-08-05 14:34:08 +0200

    Config Travis - Changement nom répertoire des classes à tester

--- TestsUnitaires/bootstrap.php
+++ TestsUnitaires/bootstrap.php
@@ -4,4 +4,4 @@ include_once('AutoLoader.php');
 // la constante __DIR__ contient le nom de répertoire du fichier courant
 // dirname(__DIR__) fournit le répertoire parent du répertoire courant
 // ceci pour remonter au répertoire racine du projet
-AutoLoader::registerDirectory(dirname(__DIR__) . '/classes');
+AutoLoader::registerDirectory(dirname(__DIR__) . '/class');
